6c439ce56c89b1b2c757710a18478064ceffa85f
Kori Roys <kori@koriroys.com>
2012-07-23 17:52:27 -0500

    some if statements pulled out

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,45 +1,39 @@
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
     if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
+      if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
+        if item.quality > 0
+          item.quality -= 1
         end
       else
         if item.quality < 50
-          item.quality += 1
+          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+            if item.sell_in < 6
+              item.quality += 3
+            elsif item.sell_in < 11
+              item.quality += 2
+            else
+              item.quality += 1
+            end
+          else
+            item.quality += 1
+          end
+        end
+      end
+
+      item.sell_in -= 1
+
+      if item.sell_in < 0
+        if item.name != "Aged Brie"
+          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+            item.quality -= 1 if item.quality > 0
+          else
+            item.quality = 0
+          end
+        else
+          if item.quality < 50
+            item.quality += 1
+          end
         end
       end
     end
