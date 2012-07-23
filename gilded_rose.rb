afb3b500114c1dc42a87cc09d3639ab0388e70a3
Kori Roys <kori@koriroys.com>
2012-07-23 18:50:14 -0500

    moved quality checks to be a final operation

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -10,24 +10,28 @@ def update_quality(items)
     when SULFURAS
       next
     when CHEESE
-      item.quality += 1 if item.quality < MAX_QUALITY
+      item.quality += 1
       item.sell_in -= 1
       if expired?(item)
-        item.quality += 1 if item.quality < MAX_QUALITY
+        item.quality += 1
       end
     when TICKETS
-      backstage_pass if item.quality < MAX_QUALITY
+      backstage_pass
       item.sell_in -= 1
       if expired?(item)
         item.quality = 0
       end
     else
-      item.quality -= 1 if item.quality > 0
       item.sell_in -= 1
       if expired?(item)
-        item.quality -= 1 if item.quality > 0
+        item.quality -= 2
+      else
+        item.quality -= 1
       end
     end
+
+    item.quality = 50 if item.quality > 50
+    item.quality = 0 if item.quality < 0
   end
 end
 
